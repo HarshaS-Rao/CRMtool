3ea0a7109ed18b3066e225e642b0a29be343bc04
Harsha S <86195317+HarshaS-Rao@users.noreply.github.com>
2023-04-19 11:49:09 +0530

    Add files via upload

---
+++ accounts_module.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    industry = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
+        return self.email
