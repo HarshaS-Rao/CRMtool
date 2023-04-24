0e88ec9fd2030e1f5aea04e794d9636157d34b0b
Neha_Hegde <54177691+Nehahegde@users.noreply.github.com>
2023-04-24 19:03:12 +0530

    Update accounts_module.py

--- accounts_module.py
+++ accounts_module.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Account(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField()
     phone = models.CharField(max_length=20, blank=True, null=True)
     website = models.URLField(blank=True, null=True)
     industry = models.CharField(max_length=50, blank=True, null=True)
